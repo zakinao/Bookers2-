5c60870ee0924ea7fb3f6843f71670360ad61726
zakinao <zakinaororo@gmail.com>
2021-01-10 09:28:05 +0000

    add user show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,29 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def create
+    @book = current_user.books.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'index'
+    end
+  end
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @books = @user.books.all
+    @book = Book.new
   end
 
   def edit
